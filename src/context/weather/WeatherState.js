b805cc7127238219f23c89085a36fc10e4ba70c9
Soulaymane Abiadou <abiadou.soulaymane@gmail.com>
2019-11-08 12:33:04 +0100

    Prepared the project for deployment

--- src/context/weather/WeatherState.js
+++ src/context/weather/WeatherState.js
@@ -3,6 +3,13 @@ import WeatherContext from './WeatherContext';
 import WeatherReducer from './WeatherReducer';
 import { SET_WEATHER } from '../types';
 
+let dsKey;
+if (process.env.NODE_ENV !== 'production') {
+  dsKey = process.env.REACT_APP_DS_KEY;
+} else {
+  dsKey = process.env.DS_KEY;
+}
+
 const WeatherState = props => {
   const initialState = {
     weather: {}
@@ -15,7 +22,7 @@ const WeatherState = props => {
     try {
       const { latitude, longitude } = position.coords;
       const res = await fetch(
-        `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${process.env.REACT_APP_DS_KEY}/${latitude},${longitude}?units=si`
+        `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${dsKey}/${latitude},${longitude}?units=si`
       );
       const data = await res.json();
       dispatch({ type: SET_WEATHER, payload: data });
